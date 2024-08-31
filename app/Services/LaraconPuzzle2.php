a9f34e06354f91f68754972202d2b560373a1b1a
Jason Klein <jason.klein@logicforte.com>
2024-08-30 21:49:57 -0500

    puzzle 2: 423 bytes (remove array init, remove whitespace)

--- app/Services/LaraconPuzzle2.php
+++ app/Services/LaraconPuzzle2.php
@@ -9,7 +9,7 @@ class LaraconPuzzle2
         // Start output buffering
         ob_start();
 
-        // SOLUTION (432 bytes)
+        // SOLUTION (423 bytes)
         // * Variable Substitution
         // * Reduce Metadata (calculate Column Names from Key Names)
         // * Remove Default Parameters
@@ -17,8 +17,10 @@ class LaraconPuzzle2
         // * Remove Whitespace
         // * Optimize Parameter
         // * Optimize Comparison
+        // * Remove Array Init
+        // * Remove Whitespace
 
-        $j=json_decode($x,1);$m=array_combine(array_keys($j[0]),[26,11,16,44]);$w=[];$y=[];foreach($m as $k=>$l){$w[]=str_repeat('-',$l);$n=ucwords(str_replace('_',' ',$k));$y[]=str_pad($n,$l-2);}$s='+'.implode('+',$w)."+\n";$t='| '.implode(' | ',$y)." |\n";$b='';foreach($j as $c){$z=[];foreach($m as $k=>$l){$v=$c[$k];if($k[0]==='b'){$v='$'.number_format($v);}$z[]=str_pad($v,$l-2);}$b.='| '.implode(' | ',$z)." |\n";}echo $s.$t.$s.$b.$s;
+        $j=json_decode($x,1);$m=array_combine(array_keys($j[0]),[26,11,16,44]);$y=[];foreach($m as$k=>$l){$w[]=str_repeat('-',$l);$n=ucwords(str_replace('_',' ',$k));$y[]=str_pad($n,$l-2);}$s='+'.implode('+',$w)."+\n";$t='| '.implode(' | ',$y)." |\n";$b='';foreach($j as$c){$z=[];foreach($m as$k=>$l){$v=$c[$k];if($k[0]==='b'){$v='$'.number_format($v);}$z[]=str_pad($v,$l-2);}$b.='| '.implode(' | ',$z)." |\n";}echo $s.$t.$s.$b.$s;
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
