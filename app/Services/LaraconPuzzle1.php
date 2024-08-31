5bb9ba376aff723aea555925e13b435246716ff0
Jason Klein <jason.klein@logicforte.com>
2024-08-30 20:41:48 -0500

    puzzle 1: 134 bytes (eliminate variable)

--- app/Services/LaraconPuzzle1.php
+++ app/Services/LaraconPuzzle1.php
@@ -9,16 +9,16 @@ class LaraconPuzzle1
         // Start output buffering
         ob_start();
 
-        // SOLUTION (140 bytes)
+        // SOLUTION (134 bytes)
         // * Variable Substitution
         // * Remove Comments
         // * Remove Variables
         // * Remove Whitespace
         // * Remove Default Parameters
         // * Fixed Line Width (instead of auto-detecting line width)
-        // * Eliminate Variable
+        // * Eliminate Variables
 
-        $e=explode("\n",$x);$i=68;foreach($e as $k=>$l){$e[$k]=strtr(rtrim(strrev(str_pad($l,$i))),'bd()<>/\\','db)(><\\/');}echo implode("\n", $e);
+        $e=explode("\n",$x);foreach($e as $k=>$l){$e[$k]=strtr(rtrim(strrev(str_pad($l,68))),'bd()<>/\\','db)(><\\/');}echo implode("\n", $e);
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
