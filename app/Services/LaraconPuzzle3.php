11c43fb0b8610444eb76524b68837b2423aebaf8
Jason Klein <jason.klein@logicforte.com>
2024-08-30 22:28:39 -0500

    puzzle 3: 322 bytes (remove comments, remove whitespace)

--- app/Services/LaraconPuzzle3.php
+++ app/Services/LaraconPuzzle3.php
@@ -9,43 +9,14 @@ class LaraconPuzzle3
         // Start output buffering
         ob_start();
 
-        // SOLUTION (648 bytes)
+        // SOLUTION (322 bytes)
         // * Variable Substitution
         // * Remove Unused Variables
         // * Remove Variables
+        // * Remove Comments
+        // * Remove Whitespace
 
-        $d = [];
-        foreach (explode("\n", $x) as $x => $l) {
-            // loop through each character in $line
-            for ($y = 0; $y < 10; $y++) {
-                $d[$x][$y] = $l[$y]??'';
-            }
-        }
-
-        // loop through all lines in reverse
-        for ($y = 10; $y >= 0; $y--) {
-            $f = 9;
-            for ($x = 9; $x >= 0; $x--) {
-                if (!empty($d[$x][$y])) {
-                    $d[$f+1][$y] = $d[$x][$y];
-                    $d[$x][$y] = '';
-                    $f--;
-                }
-            }
-        }
-
-        // consolidate data array into lines
-        $output = '';
-        for ($x = 2; $x < 11; $x++) {
-            $l = '';
-            for ($y = 0; $y <= 10; $y++) {
-                $l .= $d[$x][$y] ?? ' ';
-            }
-            $output .= trim($l)."\n";
-        }
-
-        // output
-        echo $output;
+        $d=[];foreach(explode("\n",$x)as $x=>$l){for($y=0;$y<10;$y++){$d[$x][$y]=$l[$y]??'';}}for($y=10;$y>=0;$y--){$f=9;for($x=9;$x>=0;$x--){if(!empty($d[$x][$y])){$d[$f+1][$y]=$d[$x][$y];$d[$x][$y]='';$f--;}}}$output='';for($x=2;$x<11;$x++){$l='';for($y=0;$y<=10;$y++){$l.=$d[$x][$y]?? ' ';}$output.=trim($l)."\n";}echo $output;
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
