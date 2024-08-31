c5b99e5bfad40328183da6e8911ce19b3a7188c7
Jason Klein <jason.klein@logicforte.com>
2024-08-30 21:07:41 -0500

    puzzle 2: 441 bytes (remove comments, remove whitespace)

--- app/Services/LaraconPuzzle2.php
+++ app/Services/LaraconPuzzle2.php
@@ -9,48 +9,14 @@ class LaraconPuzzle2
         // Start output buffering
         ob_start();
 
-        // SOLUTION (752 bytes)
+        // SOLUTION (441 bytes)
         // * Variable Substitution
         // * Reduce Metadata (calculate Column Names from Key Names)
         // * Remove Default Parameters
+        // * Remove Comments
+        // * Remove Whitespace
 
-        $j = json_decode($x, true);
-
-        //[
-        //    'landmark'=>26,
-        //    'city'=>11,
-        //    'build_cost'=>16,
-        //    'attendance_record'=>44,
-        //]
-        $m = array_combine(array_keys($j[0]), [26, 11, 16, 44]);
-
-        // header
-        $w = [];
-        $y = [];
-        foreach ($m as $k => $l) {
-            $w[] = str_repeat('-', $l);
-            $n = ucwords(str_replace('_', ' ', $k));
-            $y[] = str_pad($n, $l - 2, ' ', STR_PAD_RIGHT);
-        }
-        $s = '+'.implode('+', $w)."+\n";
-        $t = '| '.implode(' | ', $y)." |\n";
-
-        // body
-        $b = '';
-        foreach ($j as $c) {
-            $z = [];
-            foreach ($m as $k => $l) {
-                $v = $c[$k];
-                if ($k === 'build_cost') {
-                    $v = '$'.number_format($v);
-                }
-                $z[] = str_pad($v, $l - 2);
-            }
-            $b .= '| '.implode(' | ', $z)." |\n";
-        }
-
-        // output
-        echo $s.$t.$s.$b.$s;
+        $j=json_decode($x,true);$m=array_combine(array_keys($j[0]),[26,11,16,44]);$w=[];$y=[];foreach($m as $k=>$l){$w[]=str_repeat('-',$l);$n=ucwords(str_replace('_',' ',$k));$y[]=str_pad($n,$l-2);}$s='+'.implode('+',$w)."+\n";$t='| '.implode(' | ',$y)." |\n";$b='';foreach($j as $c){$z=[];foreach($m as $k=>$l){$v=$c[$k];if($k==='build_cost'){$v='$'.number_format($v);}$z[]=str_pad($v,$l-2);}$b.='| '.implode(' | ',$z)." |\n";}echo $s.$t.$s.$b.$s;
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
