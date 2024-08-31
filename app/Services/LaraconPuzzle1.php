b22ef788fc859bb754cf856fde4e0e45ae0e38f0
Jason Klein <jason.klein@logicforte.com>
2024-08-30 20:16:24 -0500

    puzzle 1: 220 bytes (remove whitespace)

--- app/Services/LaraconPuzzle1.php
+++ app/Services/LaraconPuzzle1.php
@@ -9,24 +9,13 @@ class LaraconPuzzle1
         // Start output buffering
         ob_start();
 
-        // SOLUTION (282 bytes)
+        // SOLUTION (220 bytes)
         // * Variable Substitution
         // * Remove Comments
         // * Remove Variables
+        // * Remove Whitespace
 
-        $e = explode("\n", $x);
-        $i = 0;
-        foreach ($e as $l) {
-            if (strlen($l) > $i) {
-                $i = strlen($l);
-            }
-        }
-        $i++;
-        foreach ($e as $k => $l) {
-            $s = str_pad($l, $i, ' ', STR_PAD_RIGHT);
-            $e[$k] = strtr(rtrim(strrev($s)), 'bd()<>/\\', 'db)(><\\/');
-        }
-        echo implode("\n", $e);
+        $e=explode("\n",$x);$i=0;foreach($e as $l){if(strlen($l)>$i){$i=strlen($l);}}$i++;foreach($e as $k=>$l){$s=str_pad($l,$i,' ',STR_PAD_RIGHT);$e[$k]=strtr(rtrim(strrev($s)),'bd()<>/\\','db)(><\\/');}echo implode("\n", $e);
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
