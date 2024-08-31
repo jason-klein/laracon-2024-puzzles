55e42e6af717b405b942e202a716ec8c26690764
Jason Klein <jason.klein@logicforte.com>
2024-08-30 22:30:39 -0500

    puzzle 3: 306 bytes (var substitution, remove whitespace)

--- app/Services/LaraconPuzzle3.php
+++ app/Services/LaraconPuzzle3.php
@@ -9,14 +9,16 @@ class LaraconPuzzle3
         // Start output buffering
         ob_start();
 
-        // SOLUTION (322 bytes)
+        // SOLUTION (306 bytes)
         // * Variable Substitution
         // * Remove Unused Variables
         // * Remove Variables
         // * Remove Comments
         // * Remove Whitespace
+        // * Variable Substitution
+        // * Remove Whitespace
 
-        $d=[];foreach(explode("\n",$x)as $x=>$l){for($y=0;$y<10;$y++){$d[$x][$y]=$l[$y]??'';}}for($y=10;$y>=0;$y--){$f=9;for($x=9;$x>=0;$x--){if(!empty($d[$x][$y])){$d[$f+1][$y]=$d[$x][$y];$d[$x][$y]='';$f--;}}}$output='';for($x=2;$x<11;$x++){$l='';for($y=0;$y<=10;$y++){$l.=$d[$x][$y]?? ' ';}$output.=trim($l)."\n";}echo $output;
+        $d=[];foreach(explode("\n",$x)as$x=>$l){for($y=0;$y<10;$y++){$d[$x][$y]=$l[$y]??'';}}for($y=10;$y>=0;$y--){$f=9;for($x=9;$x>=0;$x--){if(!empty($d[$x][$y])){$d[$f+1][$y]=$d[$x][$y];$d[$x][$y]='';$f--;}}}$o='';for($x=2;$x<11;$x++){$l='';for($y=0;$y<=10;$y++){$l.=$d[$x][$y]?? ' ';}$o.=trim($l)."\n";}echo $o;
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
