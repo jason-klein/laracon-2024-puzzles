db53a69ca844e940293dd36d83c284f615a0a88a
Jason Klein <jason.klein@logicforte.com>
2024-08-30 22:48:47 -0500

    puzzle 3: 260 bytes (remove braces, remove trim, remove var)

--- app/Services/LaraconPuzzle3.php
+++ app/Services/LaraconPuzzle3.php
@@ -9,7 +9,7 @@ class LaraconPuzzle3
         // Start output buffering
         ob_start();
 
-        // SOLUTION (276 bytes)
+        // SOLUTION (260 bytes)
         // * Variable Substitution
         // * Remove Unused Variables
         // * Remove Variables
@@ -22,8 +22,11 @@ class LaraconPuzzle3
         // * Remove Whitespace
         // * Refactor Decrement
         // * Remove Variable
+        // * Remove Braces
+        // * Remove trim()
+        // * Remove Variable
 
-        foreach(explode("\n",$x)as$x=>$l)for($y=0;$y<10;$y++)$d[$x][$y]=$l[$y]??'';for($y=10;$y>=0;$y--){$f=9;for($x=9;$x>=0;$x--){if(!empty($d[$x][$y])){$d[$f--+1][$y]=$d[$x][$y];$d[$x][$y]='';}}}for($x=2;$x<11;$x++){$l='';for($y=0;$y<=10;$y++)$l.=$d[$x][$y]??'';echo trim($l)."\n";}
+        foreach(explode("\n",$x)as$x=>$l)for($y=0;$y<10;$y++)$d[$x][$y]=$l[$y]??'';for($y=10;$y>=0;$y--){$f=9;for($x=9;$x>=0;$x--)if(!empty($d[$x][$y])){$d[$f--+1][$y]=$d[$x][$y];$d[$x][$y]='';}}for($x=2;$x<11;$x++){for($y=0;$y<=10;$y++)echo $d[$x][$y]??'';echo "\n";}
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
