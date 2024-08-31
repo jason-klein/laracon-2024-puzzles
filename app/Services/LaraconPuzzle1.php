40720afec4bcf5210baacf0810da0e26822ae294
Jason Klein <jason.klein@logicforte.com>
2024-08-30 20:25:48 -0500

    puzzle 1: 146 bytes (fixed line width)

--- app/Services/LaraconPuzzle1.php
+++ app/Services/LaraconPuzzle1.php
@@ -9,14 +9,15 @@ class LaraconPuzzle1
         // Start output buffering
         ob_start();
 
-        // SOLUTION (202 bytes)
+        // SOLUTION (146 bytes)
         // * Variable Substitution
         // * Remove Comments
         // * Remove Variables
         // * Remove Whitespace
         // * Remove Default Parameters
+        // * Fixed Line Width (instead of auto-detecting line width)
 
-        $e=explode("\n",$x);$i=0;foreach($e as $l){if(strlen($l)>$i){$i=strlen($l);}}$i++;foreach($e as $k=>$l){$s=str_pad($l,$i);$e[$k]=strtr(rtrim(strrev($s)),'bd()<>/\\','db)(><\\/');}echo implode("\n", $e);
+        $e=explode("\n",$x);$i=68;foreach($e as $k=>$l){$s=str_pad($l,$i);$e[$k]=strtr(rtrim(strrev($s)),'bd()<>/\\','db)(><\\/');}echo implode("\n", $e);
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
