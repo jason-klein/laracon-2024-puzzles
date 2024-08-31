dd2f5147b28997fe8a01eb3c9e79f98090ca301d
Jason Klein <jason.klein@logicforte.com>
2024-08-30 19:57:55 -0500

    puzzle 1: 282 bytes (variable substitution, remove comments)

--- app/Services/LaraconPuzzle1.php
+++ app/Services/LaraconPuzzle1.php
@@ -9,32 +9,24 @@ class LaraconPuzzle1
         // Start output buffering
         ob_start();
 
-        // SOLUTION (684 bytes)
-        $lines = explode("\n", $x);
-        $length = 0;
-        foreach ($lines as $line) {
-            if (strlen($line) > $length) {
-                $length = strlen($line);
+        // SOLUTION (282 bytes)
+        // * Variable Substitution
+        // * Remove Comments
+        // * Remove Variables
+
+        $e = explode("\n", $x);
+        $i = 0;
+        foreach ($e as $l) {
+            if (strlen($l) > $i) {
+                $i = strlen($l);
             }
         }
-        $length++;
-
-        foreach ($lines as $key => $line) {
-            // pad all lines to the same length
-            $string = str_pad($line, $length, ' ', STR_PAD_RIGHT);
-            // reverse the line
-            $string = strrev($string);
-            // trim whitespace from RIGHT side of the line
-            $string = rtrim($string);
-            // replace all '(' with ')' and vice versa
-            // replace all '/' with '\' and vice versa
-            // replace all '<' with '>' and vice versa
-            $string = strtr($string, 'bd()<>/\\', 'db)(><\\/');
-
-            $lines[$key] = $string;
+        $i++;
+        foreach ($e as $k => $l) {
+            $s = str_pad($l, $i, ' ', STR_PAD_RIGHT);
+            $e[$k] = strtr(rtrim(strrev($s)), 'bd()<>/\\', 'db)(><\\/');
         }
-
-        echo implode("\n", $lines);
+        echo implode("\n", $e);
 
         // Return output buffer and stop output buffering
         return ob_get_clean();
